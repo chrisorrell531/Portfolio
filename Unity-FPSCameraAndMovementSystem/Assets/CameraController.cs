a883157f09f3af2c0fb3265e5b0e0d57e0e416f2
chrisorrell531 <57040644+chrisorrell531@users.noreply.github.com>
2020-10-24 22:07:11 +0100

    Add files via upload

---
+++ Unity-FPSCameraAndMovementSystem/Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿/// Programmed by Christopher Philip Orrell 18/05/2018
+/// This script controls the camera's movement, and makes as if its an FPS, Game.
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Creates variables one is a constant atm becuase it doesnt change but it could  change... and the other saves the y rotation
+    float y_Rotation;
+    float turnSpeed = 3f;
+    //Creates a variable for the player
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This creates a variable called x rotation and sets it the the camera x rotation plus the input from the mouses x axis multipled by the turn speed. to give an integer answer.
+        float x_Rotation = transform.eulerAngles.y + Input.GetAxis("Mouse X") * turnSpeed;
+        //This adds y rotation the value given from the mouses y axis multiplied by the turn speed.
+        y_Rotation += Input.GetAxis("Mouse Y") * turnSpeed;
+        //Now we clamp this angle so the camera cant over turn.
+        y_Rotation = Mathf.Clamp(y_Rotation, -60f, 60f);
+        //Now we apply the y rotation to the camera like a head looking up and down, we rotate the whole body instead to look left and right.
+        transform.localEulerAngles = new Vector3(-y_Rotation, 0, 0);
+        player.transform.localEulerAngles = new Vector3(0, x_Rotation, 0);
+    }
+}
---
+++ Unity-FPSCameraAndMovementSystem/Assets/FPSMovement.cs
@@ -0,0 +1,53 @@
+﻿/// Programmed By Christopher Orrell 18/05/2018.
+/// This script controlls the player movement.
+
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    //Creates variables for the players movement speed, the Horizontal Input, Vertical Input, the Controller to the move the player etc.
+    public float speed = 1f;
+    public float upwardForce = 4f;
+    float hori_Input;
+    float verti_Input;
+    float upward_input;
+    Vector3 moveDir;
+    CharacterController controller;
+    Vector3 forward;
+    Vector3 right;
+    Vector3 upward;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hori_Input = Input.GetAxis("Horizontal") * speed;
+        verti_Input = Input.GetAxis("Vertical") * speed;
+        if (Input.GetButtonUp("Jump"))
+        {
+            upward_input = upwardForce;
+        }
+        upward = transform.up;
+        forward = transform.forward;
+        right = transform.right;
+        upward_input -= 9.89f * Time.deltaTime;
+    }
+
+    //This is inline with the physics timestep, so anything physics based such as movement... collisions etc
+    private void FixedUpdate()
+    {
+        //Moves the character controller component forward multipled by our vertical input from W or A or Up arrow or Down arrow added by our right direction multiplied by our horizontal input of A or D or Left arrow and Right Arrow keys.
+        controller.SimpleMove(forward * verti_Input + right * hori_Input);
+        //Moves the character upward as if the character has jumped.
+        if (upward_input > 0)
+        {
+            controller.Move((upward * upwardForce) * Time.deltaTime);
+        }
+    }
+}
